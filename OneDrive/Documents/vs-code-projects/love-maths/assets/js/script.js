d0df400f1c90da2da86aa01dd77c33345cedd8c4
rory-codes <rory.thompson@outlook.com>
2025-06-01 13:47:23 +0100

    Add code to increment score and incorrect answers tally

--- OneDrive/Documents/vs-code-projects/love-maths/assets/js/script.js
+++ OneDrive/Documents/vs-code-projects/love-maths/assets/js/script.js
@@ -49,8 +49,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! you got it right :D")
+        incrementScore ();
     } else {
         alert(`awwwww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculateCorrectAnswer[1]);
@@ -73,12 +75,19 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
-
+/**
+ * Gets the current tally of incorrect answers from the DOM and incremenets iy by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
